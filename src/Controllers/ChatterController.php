6b3268f43f4c056eda8da5452e216f400137e590
Sergei Ten <sergei.ten@gmail.com>
2018-01-15 10:18:21 +0900

    Add search function

--- src/Controllers/ChatterController.php
+++ src/Controllers/ChatterController.php
@@ -4,21 +4,14 @@ namespace DevDojo\Chatter\Controllers;
 
 use Auth;
 use DevDojo\Chatter\Models\Models;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as Controller;
 
 class ChatterController extends Controller
 {
-    public function index($slug = '')
+    public function index(Request $request, $slug = '')
     {
-        $pagination_results = config('chatter.paginate.num_of_results');
-
-        $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->orderBy('created_at', 'DESC')->paginate($pagination_results);
-        if (isset($slug)) {
-            $category = Models::category()->where('slug', '=', $slug)->first();
-            if (isset($category->id)) {
-                $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->where('chatter_category_id', '=', $category->id)->orderBy('created_at', 'DESC')->paginate($pagination_results);
-            }
-        }
+        $discussions = $this->getDiscussions($slug, $request->get('query'), config('chatter.paginate.num_of_results'));
 
         $categories = Models::category()->all();
         $chatter_editor = config('chatter.editor');
@@ -29,6 +22,35 @@ class ChatterController extends Controller
         }
 
         return view('chatter::home', compact('discussions', 'categories', 'chatter_editor'));
+    }
+
+    private function getDiscussions($slug = '', $search, $pagination_results) {
+        $query = Models::discussion()
+            ->with('user')
+            ->with('post')
+            ->with('postsCount')
+            ->with('category');
+
+        if ($query) {
+            $query->where(function($q) use ($search) {
+                $q->where('title', 'like', '%' . $search . '%');
+                $q->orWhereHas('post', function($q) use ($search) {
+                    $q->where('body', 'like', '%' . $search . '%');
+                });
+            });
+        }
+
+        if (!empty($slug)) {
+            $category = Models::category()->where('slug', '=', $slug)->first();
+
+            if ($category) {
+                $query->where('chatter_category_id', '=', $category->id);
+            }
+        }
+
+        return $query
+            ->orderBy('created_at', 'DESC')
+            ->paginate($pagination_results);
     }
 
     public function login()
